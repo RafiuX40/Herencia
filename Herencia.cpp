bce1c6355861e65908eaed9e13f9349daadc8139
Rafael Ramos <22090@portalcedi.edu.mx>
2024-03-11 13:43:20 -0600

    agregar codigo

--- Herencia.cpp
+++ Herencia.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+class X
+{
+
+    public:
+    char c;
+
+};
+
 class A
 {
 private:
@@ -9,6 +17,11 @@ private:
 
 public:
     int x;
+
+    void sumarX(int s)
+    {
+        this->x += s;
+    }
 };
 
 class B : public A
@@ -16,6 +29,12 @@ class B : public A
 
 public:
     int y;
+};
+
+class C : public B, public X
+{
+public:
+    int w;
 };
 
 int main()
@@ -26,6 +45,13 @@ int main()
     a.x = 5;
     b.y = 10;
     b.x = 7;
+
+    cout << b.x << endl;
+    b.sumarX(15);
+    cout << a.x << endl;
+
+    C c;
+    
 
     return 0;
 }
